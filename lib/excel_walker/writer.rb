7995bd16adfabd99ebb7461048d3abc6789327d3
Shadab Ahmed <shadab.ansari@gmail.com>
2014-05-11 08:59:39 +0530

    Updated writer

--- lib/excel_walker/writer.rb
+++ lib/excel_walker/writer.rb
@@ -12,15 +12,17 @@ module ExcelWalker
 
     class Writer
       def initialize(file_path)
-
+        @file_path = file_path
+        @package = Axlsx::Package.new
+        @workbook = @package.workbook
       end
 
       def new_sheet(sheet_name)
-
+        SheetBuilder.new(@workbook, sheet_name)
       end
 
       def save
-
+        @package.serialize @file_path
       end
     end
   end
